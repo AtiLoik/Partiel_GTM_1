ccc77c7b9cfef999c70a7f49c2d136183dec3f8d
loik froelich <loikfr@live.fr>
2020-01-22 11:11:51 +0100

    Ajout des fichiers de base

---
+++ Connexion.php
@@ -0,0 +1,6 @@
+<?php
+	$host="mysql:host=localhost;dbname=partiel_1_GTM";
+	$user="root";
+	$mdp="";
+	$pdo = new PDO($host, $user, $mdp);
+?>
---
+++ Index.php
@@ -0,0 +1,13 @@
+<?php
+    include 'Connexion.php';
+    include 'Classes/Etudiants.php';
+?>
+
+<h1>Bienvenue sur le site d'acceuil de gestion de l'ENSUP</h1>
+
+<a href="#">Créer un étudiant</a></br>
+<a href="#">Associer un cours à un étudiant</a></br>
+<a href="#">Lire les informations d'un étudiant</a></br>
+<a href="#">Modifier un étudiant</a></br>
+<a href="#">Supprimer un étudiant</a></br>
+<a href="#">Lister les étudiants</a></br>
